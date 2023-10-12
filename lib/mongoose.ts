057e1233376700f934fba6dba3dd93b45f1400ce
med-aziz-guennichi <medazizguennichi@gmail.com>
2023-10-12 22:47:59 +0100

    first commit

---
+++ lib/mongoose.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) return console.log("Please set MONGODB_URL");
+  if (isConnected) return console.log("Already connected to DB");
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    isConnected = true;
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log(error);
+  }
+};
