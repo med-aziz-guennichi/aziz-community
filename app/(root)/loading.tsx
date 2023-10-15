b75b8bef281d95916d1259537688873626cb2c75
med-aziz-guennichi <medazizguennichi@gmail.com>
2023-10-15 20:55:31 +0100

    add loading and animations

--- app/(root)/loading.tsx
+++ app/(root)/loading.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { ChakraProvider, Spinner } from "@chakra-ui/react";
+import Load from "@/animations/animation_lnrvvjg6.json";
+import Lotti from "lottie-react";
 
 const loading = () => {
   return (
     <ChakraProvider>
       <div className="fixed inset-0 flex items-center justify-center z-50">
         <div className="fixed inset-0 backdrop-blur-md backdrop-filter bg-opacity-50"></div>
-        <Spinner color="red.500" speed="0.65s" />
+        {/* <Spinner color="red.500" speed="0.65s" /> */}
+        <Lotti animationData={Load} />
       </div>
     </ChakraProvider>
   );
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,12 +1,19 @@
+"use client";
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { dark } from "@clerk/themes";
+import Anim from "@/animations/animation_lnrpfypc.json";
+import Lotti from "lottie-react";
 function Topbar() {
   return (
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
-        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        {/* <Image src="/assets/logo.svg" alt="logo" width={28} height={28} /> */}
+        <div className="w-[90px]">
+          <Lotti animationData={Anim} size={16} loop={false} />
+        </div>
+
         <p className="text-heading3-bold text-light-1 max-xs:hidden">
           Aziz threads
         </p>
