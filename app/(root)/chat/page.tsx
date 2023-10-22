f27ee7a1e2aaceb8fa6c57f2564da48e29f66ce6
med-aziz-guennichi <medazizguennichi@gmail.com>
2023-10-23 00:17:21 +0100

    add the apk

--- app/(root)/chat/page.tsx
+++ app/(root)/chat/page.tsx
@@ -2,6 +2,13 @@ const Page = () => {
   return (
     <div>
       <h1 className="text-white">This is Chat Page</h1>
+      <a
+        href="/app-armeabi-v7a-release.apk"
+        download={"app-armeabi-v7a-release.apk"}
+        className="text-white"
+      >
+        DOwnload the apk
+      </a>
     </div>
   );
 };
